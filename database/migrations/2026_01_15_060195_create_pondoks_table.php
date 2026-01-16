1170ad5174b358f67ebeb85f7b456c660eb28e53
Asatidz <fahri.dwi10096@smk.belajar.id>
2026-01-17 06:26:25 +0700

    first commit

---
+++ database/migrations/2026_01_15_060195_create_pondoks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('pondoks', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_pondok')->unique();
+            $table->string('nama_pondok');
+            $table->string('yayasan_mitra')->nullable();
+            $table->string('jenis'); // Putra / Putri / Campuran
+            $table->string('pengasuh')->nullable();
+            // $table->integer('kapasitas')->default(0);
+            $table->boolean('is_aktif')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pondoks');
+    }
+};
